922a201c37c2a51c5ef2364b804691e61c515755
Aganapathi <47145941+Aganapathi@users.noreply.github.com>
2019-08-13 02:23:00 -0700

    Create Ash.java

---
+++ Ash.java
@@ -0,0 +1,20 @@
+    
+import java.util.Scanner;
+class Ash {
+public static void main(String arg[]) {
+int a,b,c;
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+for(a=1;a<=n;a++){
+for(b=1;b<=a;b++){
+
+System.out.print('*');
+}
+for(c=a+1;c<=n;c++){
+
+System.out.print("");
+}
+System.out.println( );
+}
+}
+}
